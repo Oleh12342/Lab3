ce7c1961e3cbc738acadefa52fd52550399fe86a
Oleh12342 <148885488+Oleh12342@users.noreply.github.com>
2025-10-02 20:28:29 +0300

    Add files via upload

--- Lab3/script/scriptreglog.js
+++ Lab3/script/scriptreglog.js
@@ -1,15 +1,17 @@
 function openLogin() {
-  document.getElementById('loginModal').hidden = false;
+  document.getElementById("loginModal").hidden = false;
+  document.getElementById("registerModal").hidden = true;
 }
 
 function closeLogin() {
-  document.getElementById('loginModal').hidden = true;
+  document.getElementById("loginModal").hidden = true;
 }
 
 function openRegister() {
-  document.getElementById('registerModal').hidden = false;
+  document.getElementById("registerModal").hidden = false;
+  document.getElementById("loginModal").hidden = true;
 }
 
 function closeRegister() {
-  document.getElementById('registerModal').hidden = true;
+  document.getElementById("registerModal").hidden = true;
 }
